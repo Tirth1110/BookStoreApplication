ad46353bfcf0a1026bf4493d68ffa19026c7787a
Tirth Shah <tirthshah111099@gmail.com>
2022-04-03 15:23:15 +0530

    Add Area as Admin & Financial

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -126,9 +126,15 @@ namespace BookStore
                 #region Conventional Routing Ex.
 
                 //endpoints.MapDefaultControllerRoute();
+                //endpoints.MapControllerRoute(
+                //    name: "Default",
+                //    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+                //for Admin Area 
                 endpoints.MapControllerRoute(
-                    name: "Default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                  name: "MyArea",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
 
                 #region Multi Optional Parameter
                 //endpoints.MapControllerRoute(
